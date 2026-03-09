ed940ff88877eaaa539574518629447e93cdb6e2
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-09 13:53:27 -0300

    Exercicio 086 Concluido

---
+++ mundopython3/aula_18_listas_compostas/desafios_18/desafio_086.py
@@ -0,0 +1,13 @@
+"""
+Desafio 086: Manipulação de listas bidimensionais através de loops aninhados para controle de índices.
+"""
+matriz = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ]
+
+for indice in range(0,3):
+    for contador in range(0,3):
+        matriz[indice][contador] = int(input("Digite um valor: "))
+
+for indice in range(0,3):
+    for contador in range(0,3):
+        print(f"{matriz[indice][contador]:^5}",  end="")
+    print()
