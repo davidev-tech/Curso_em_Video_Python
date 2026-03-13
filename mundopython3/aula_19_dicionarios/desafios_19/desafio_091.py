c3481ab0c5d357758264ba326886cda32c040ac0
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-13 15:59:39 -0300

    Exercicio 091 Concluido - Ordenação de dicionarios

---
+++ mundopython3/aula_19_dicionarios/desafios_19/desafio_091.py
@@ -0,0 +1,15 @@
+from random import randint
+from time import sleep
+from operator import itemgetter
+
+jogos = {"jogador1": randint(1,6),
+        "jogador2": randint(1,6),
+        "jogador3": randint(1,6),
+        "jogador4": randint(1,6)
+       }
+
+ranking = sorted(jogos.items(), key=itemgetter(1), reverse= True)
+print("Ranking dos jogadores: ")
+for i, v in enumerate(ranking):
+    print(f"{i + 1}° lugar: {v[0]} com {v[1]} Pontos.")
+    sleep(1)
