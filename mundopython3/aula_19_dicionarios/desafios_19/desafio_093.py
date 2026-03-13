41b3c017dc29f66975349be98da3b67447d25c0f
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-13 20:41:46 -0300

    Exercicio 093 - Pequenos ajustes de exibição

--- mundopython3/aula_19_dicionarios/desafios_19/desafio_093.py
+++ mundopython3/aula_19_dicionarios/desafios_19/desafio_093.py
@@ -9,7 +9,7 @@ gols_partida = []
 total_gols = 0
 
 ficha_jogador['nome'] = str(input("Qual o nome do Jogador: "))
-ficha_jogador['partidas_jogadas'] = int(input("Quantas partidas foram jogadas? "))
+ficha_jogador['partidas_jogadas'] = int(input(f"Quantas partidas {ficha_jogador['nome']} jogou? "))
 
 for partida in range(ficha_jogador['partidas_jogadas']):
     gols = int(input(f"Quantos gols na {partida + 1}° Partida? "))
@@ -18,7 +18,9 @@ for partida in range(ficha_jogador['partidas_jogadas']):
 ficha_jogador['gols_partida'] = gols_partida[:]
 ficha_jogador['total_gols'] = sum(gols_partida)
 
-print("-=" * 15)
+print("-=" * 18)
+print(f"O jogador {ficha_jogador['nome']} jogou {ficha_jogador['partidas_jogadas']} Partidas.")
 for i, v in enumerate(ficha_jogador['gols_partida']):
-    print(f"Na {i + 1}° Partida {ficha_jogador['nome']} fez {v} gols.")
+    print(f"    => Na {i + 1}° Partida fez {v} gols.")
+print("-=" * 18)
 print(f"{ficha_jogador['nome']} teve um total de {ficha_jogador['total_gols']} em {ficha_jogador['partidas_jogadas']} Partidas.")
