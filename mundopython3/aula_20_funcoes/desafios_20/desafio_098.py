67c9a655a6da5d29883efe43f867824cf5c01913
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-27 20:21:37 -0300

    Adicionando mais interação no exercicio 098.

--- mundopython3/aula_20_funcoes/desafios_20/desafio_098.py
+++ mundopython3/aula_20_funcoes/desafios_20/desafio_098.py
@@ -1,13 +1,18 @@
+from time import sleep
+
+
 def exibir_contagem(inicio, fim, salto):
     '''Exibindo Progressão Aritmédica'''
     if salto <= 0:
         salto = 1
     if inicio < fim:
         for contador in range(inicio, fim + 1, salto):
-            print(f"{contador}", end=" ")
+            print(f"{contador}", end=" ", flush=True)
+            sleep(1)
     else:
         for contador in range(inicio, fim - 1, - salto):
-            print(f"{contador}", end=" ")
+            print(f"{contador}", end=" ", flush=True)
+            sleep(1)
     print("FIM!")
     print("-=" * 20)
 
@@ -15,7 +20,6 @@ def exibir_contagem(inicio, fim, salto):
 print("-=" * 20)
 print("Contagem de 1 a 10 de 1 em 1.")
 exibir_contagem(1, 10, 1)
-print("-=" * 20)
 print("Contagem de 10 a 0 de 2 em 2.")
 exibir_contagem(10, 0, 2)
 contagem = []
