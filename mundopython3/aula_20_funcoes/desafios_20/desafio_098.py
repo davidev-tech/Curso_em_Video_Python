671c8f84f1ffa9124a8b2f9f091373f88bca16be
Davi-dev19 <davi.gomes.dev19@gmail.com>
2026-03-27 20:12:01 -0300

    Exercicio 098 Concluido - Trabalhando PGA junto com funções

---
+++ mundopython3/aula_20_funcoes/desafios_20/desafio_098.py
@@ -0,0 +1,28 @@
+def exibir_contagem(inicio, fim, salto):
+    '''Exibindo Progressão Aritmédica'''
+    if salto <= 0:
+        salto = 1
+    if inicio < fim:
+        for contador in range(inicio, fim + 1, salto):
+            print(f"{contador}", end=" ")
+    else:
+        for contador in range(inicio, fim - 1, - salto):
+            print(f"{contador}", end=" ")
+    print("FIM!")
+    print("-=" * 20)
+
+
+print("-=" * 20)
+print("Contagem de 1 a 10 de 1 em 1.")
+exibir_contagem(1, 10, 1)
+print("-=" * 20)
+print("Contagem de 10 a 0 de 2 em 2.")
+exibir_contagem(10, 0, 2)
+contagem = []
+print("Agora é a sua vez de personalizar a contagem: ")
+contagem.append(int(input("Qual número marcara o inicio da contagem? ")))
+contagem.append(int(input("Qual número marcara o fim da contagem? ")))
+contagem.append(int(input("Qual o tamanhos do salto? ")))
+print("-=" * 20)
+print(f"Contagem de {contagem[0]} até {contagem[1]} de {contagem[2]} em {contagem[2]}")
+exibir_contagem(*contagem)
